f1b4bfff2c66af85f85fce8c9cbd0c5a0a477a79
bubu139 <giakhanhtran88@gmail.com>
2025-10-31 16:40:27 +0700

    Delete frontend_nextjs/src directory

--- frontend_nextjs/src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
-// ... (hàm 'cn' của bạn)
-
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000';
